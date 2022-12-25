7723c2adf9b389920a560fcf5895b6aef78f0350
qiweiii <yangqiwei97@gmail.com>
2022-12-25 20:03:56 +0800

    fix: health check

--- src/restful/index.ts
+++ src/restful/index.ts
@@ -1,16 +1,26 @@
 import Router from '@koa/router';
+import { Context } from 'koa';
 import { onHealthCheck } from './health';
 
 // TODO: add your restful api here
 const createRestfulRoute = (router: Router) => {
-  router.get('/health', onHealthCheck);
-
-  router.post('/your/endpoint', async (ctx) => {
-    let body = ctx.request.body;
-    if (typeof body === 'string') {
-      body = JSON.parse(body);
+  router.get('/health', async (ctx: Context) => {
+    try {
+      await onHealthCheck(ctx);
+      ctx.status = 200;
+      ctx.body = 'Success';
+    } catch (e) {
+      ctx.logger.error(e);
+      ctx.status = 500;
     }
   });
+
+  // router.post('/your/endpoint', async (ctx) => {
+  //   let body = ctx.request.body;
+  //   if (typeof body === 'string') {
+  //     body = JSON.parse(body);
+  //   }
+  // });
 };
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
